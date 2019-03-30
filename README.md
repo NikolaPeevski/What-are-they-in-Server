6f5ac0a810d5af551748ed5f1e38e40340d22002
nikolapeevski <Jeratull@hotmail.com>
2019-03-29 21:02:12 -0500

    (type:chore) Update ReadME file

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
-# Server code for What Are They In
+# What Are They In - Server side
+
+## Disclamer
+This is early work in progress with a lot of changes expected, including this README file.
+
+## Introduction
+
+This is the server side code base. 
+It is build over using Express, Parse, Python for both machine learning and webscraping.
+It is a RESTful API that invokes python scripts that return processed image data or gather information based on an actor.
+More to be added later in development to this section.
+
+## Requirements
+
+ * Latest version of NodeJS
+ * Latest version of NPM
 
 ## Setup
-
-  * clone repository
-  * run `npm install`
+* git clone
+* run `npm install`
+* run `npm start`
+* Press `CTRL + C` to stop the Express server.
 
 ## Testing
-
   *	run `npm test`
--- dist/index.js
+++ dist/index.js
@@ -4,22 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const ParseServer = require('parse-server').ParseServer;
 const app = express_1.default();
-// const api = new ParseServer({
-//   databaseURI: 'mongodb://localhost:27017/dev', // Connection string for your MongoDB database
-//   // cloud: '/home/myApp/cloud/main.js', // Absolute path to your Cloud Code
-//   appId: 'myAppId',
-//   masterKey: 'myMasterKey', // Keep this key secret!
-//   fileKey: 'optionalFileKey',
-//   serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
-// });
 const port = 8080; // default port to listen
-// app.use('/parse', api);
-// app.listen(1337, () => {
-//   console.log('parse-server-example running on port 1337.');
-// });
-// define a route handler for the default home page
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
