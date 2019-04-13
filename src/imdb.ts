ed8e613481812c596d0c69a351c6b46319f31475
nikolapeevski <Jeratull@hotmail.com>
2019-04-12 20:08:54 -0500

    (type:refactor) Add constants

--- src/imdb.ts
+++ src/imdb.ts
@@ -1,13 +1,44 @@
-import * as Promise from 'bluebird';
+import Promise from 'bluebird';
 import Constants from './constants';
+import {rejects} from "assert";
 
 class imdbModule {
 
     constructor(){}
 
+    // public getInfo(name: String) : Promise<any> {
+    //     return new Promise<any>((resolve, reject) => {
+    //         this.search(name).then(result => {
+    //         if ()
+    //         });
+    //     });
+    // }
+    //
+    // public search(name: String): Promise<any> {
+    //     return new Promise<any>( (resolve, reject) => {
+    //         let options = {
+    //
+    //         }
+    //         resolve({})
+    //     });
+    // }
+
 
 }
-
+// async function search(name) {
+//     var options = {
+//         method: 'GET',
+//         url: `${BASE_URL}/search/person?query=${name}&page=1&api_key=${API_KEY}`,
+//         json: true
+//     };
+//
+//     try {
+//         return await rp(options);
+//     } catch(err) {
+//         console.error(err);
+//         return null;
+//     }
+// }
 //
 // async function getInfo(name) {
 //     let info = {};
